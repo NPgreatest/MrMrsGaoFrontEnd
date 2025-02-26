b6921ef05f4528922e33f96903decfe9330b2aa1
NP_123 <38424@sina.com>
2025-02-26 03:54:37 -0600

    Fix Vite hosting for Heroku

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  tailwindcss()],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,  // Use Heroku's port
+    strictPort: true,                // Ensures Vite doesn't try random ports
+    hmr: {
+      clientPort: 443,                // Fix HMR for Heroku
+    },
+    allowedHosts: ['.herokuapp.com'], // Allow all Heroku subdomains
+  }
 
 
 })
